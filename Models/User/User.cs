998ad0dc9ac0538b6fc98ffe6d9c0ab7ecd245a0
KrySoSad <duythuc070205@gmail.com>
2025-07-02 21:15:35 +0700

    feat: Add properties to User and CreateUserDto for email, password, and timestamps

--- Models/User/User.cs
+++ Models/User/User.cs
@@ -1,9 +1,14 @@
 using System.ComponentModel.DataAnnotations;
 
 namespace ChatAppMini.Models;
+
 public class User
 {
     [Key]
     public Guid Id { get; set; } = Guid.NewGuid();
     public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
 }
--- Models/User/UserDTO.cs
+++ Models/User/UserDTO.cs
@@ -1,4 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 public class CreateUserDto
 {
+    [Required]
+    [StringLength(30, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 30 characters.")]
     public string Name { get; set; } = null!;
+
+    [Required]
+    [EmailAddress(ErrorMessage = "Invalid email address.")]
+    public string Email { get; set; } = null!;
+
+    [Required]
+    [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters long.")]
+    public string Password { get; set; } = null!;
 }
