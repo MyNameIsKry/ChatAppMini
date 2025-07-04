4780fbe888aa62bc41876f95060b175c5b8e4eff
KrySoSad <duythuc070205@gmail.com>
2025-07-04 18:28:49 +0700

    feat: Implement RequestLoginDto and AuthService for user login functionality

---
+++ DTOs/Auth/ResponseLoginDTO.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ChatAppMini.DTOs.Auth;
+public class ResponseLoginDto
+{
+    public Guid Id { get; set; }
+    
+    public string Name { get; set; } = null!;
+    
+    public string Email { get; set; } = null!;
+
+    public string AccessToken { get; set; } = null!;
+
+    public string RefreshToken { get; set; } = null!;
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+}
---
+++ Services/AuthService.cs
@@ -0,0 +1,50 @@
+using ChatAppMini.DTOs.User;
+using Utils;
+using ChatAppMini.DTOs.Auth;
+using ChatAppMini.Models;
+
+public interface IAuthService
+{
+    Task<ServiceResult<ResponseLoginDto>> LoginAsync(RequestLoginDto user);
+}
+
+public class AuthService : IAuthService
+{
+    private readonly IUserRepository _repo;
+
+    public AuthService(IUserRepository repo)
+    {
+        _repo = repo;
+    }
+
+    public async Task<ServiceResult<ResponseLoginDto>> LoginAsync(RequestLoginDto user)
+    {
+        if (user == null || string.IsNullOrEmpty(user.Email) || string.IsNullOrEmpty(user.Password))
+        {
+            return ServiceResult<ResponseLoginDto>.Fail("Invalid login data provided.");
+        }
+
+        User? existingUser = await _repo.GetUsersByEmailAsync(user.Email);
+        if (existingUser == null)
+        {
+            return ServiceResult<ResponseLoginDto>.Fail("Email or password is incorrect.");
+        }
+
+        bool isPasswordValid = HashPasswordUtil.VerifyPassword(user.Password, existingUser.Password);
+        if (!isPasswordValid)
+        {
+            return ServiceResult<ResponseLoginDto>.Fail("Email or password is incorrect.");
+        }
+
+        ResponseLoginDto responseLogin = new ResponseLoginDto
+        {
+            Id = existingUser.Id,
+            Name = existingUser.Name,
+            Email = existingUser.Email,
+            CreatedAt = existingUser.CreatedAt,
+            UpdatedAt = existingUser.UpdatedAt
+        };
+
+        return ServiceResult<ResponseLoginDto>.Success(responseLogin);
+    }
+}
