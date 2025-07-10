2fa5cdcb001a6bc55627bb1a7c5e9576fc8112f2
KrySoSad <duythuc070205@gmail.com>
2025-07-10 21:02:51 +0700

    init chat app client

---
+++ ChatAppMini-Client/src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Chat App Mini",
+  description: "Using NextJS v15, ASP.NET SignalR",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={``}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
