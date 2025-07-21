dab16d19371992f6e723602188438a3e0fe7672d
KrySoSad <duythuc070205@gmail.com>
2025-07-21 18:28:46 +0700

    refactor: simplify ResponseMessageDTO by removing unused properties

--- ChatAppMini-Server/DTOs/Message/ResponseMessageDTO.cs
+++ ChatAppMini-Server/DTOs/Message/ResponseMessageDTO.cs
@@ -1,11 +1,8 @@
 public class ResponseMessageDTO
 {
+    public Guid Id { get; set; }
     public Guid SenderId { get; set; }
-
-    public Guid? ReceiverId { get; set; }
-
-    public string? GroupId { get; set; }
-
     public string Content { get; set; } = null!;
-    public DateTime SentAt { get; set; }    
+    public DateTime SentAt { get; set; }
+
 }
