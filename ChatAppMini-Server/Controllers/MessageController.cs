b6000e9abe3da6eb28a7493759c33f4a3ad45a43
KrySoSad <duythuc070205@gmail.com>
2025-07-23 12:27:20 +0700

    feat: refactor SendMessageAsync method in MessageController for improved message handling

--- ChatAppMini-Server/Controllers/MessageController.cs
+++ ChatAppMini-Server/Controllers/MessageController.cs
@@ -13,43 +13,20 @@ public class MessageController : ControllerBase
 
     public MessageController(IMessageService messageService) => _messageService = messageService;
 
+    [HttpPost("send/{conversationId}")]
     [Authorize]
-    [HttpPost]
-    public async Task<ApiResponse<Message>> SaveUsersMessages(RequestMessageDTO msg)
+    public async Task<ApiResponse<ResponseMessageDTO>> SendMessageAsync(Guid conversationId, [FromBody] RequestMessageDTO messageDto)
     {
-        try
+        if (messageDto == null)
         {
-            ServiceResult<Message> msgResult = await _messageService.SaveUsersMessages(msg);
-
-            if (msgResult.IsSuccess)
-                return new ApiResponse<Message>(201, "Sent", msgResult.Data);
-
-            return new ApiResponse<Message>(400, msgResult.Message);
-
+            return new ApiResponse<ResponseMessageDTO>(400, "Message content cannot be null.");
         }
-        catch (Exception err)
-        {
-            Logger.LogError("Error saving new messages", err);
-            return new ApiResponse<Message>(500, "An error occur when sent messages");
-        }
-    }
 
-    [HttpGet("{id}")]
-    [Authorize]
-    public async Task<ApiResponse<List<Message>>> GetUsersMessages(Guid id)
-    {
-        try
-        {
-            ServiceResult<List<Message>> messagesResult = await _messageService.GetUsersMessages(id);
-            if (!messagesResult.IsSuccess)
-                return new ApiResponse<List<Message>>(400, messagesResult.Message);
+        ServiceResult<ResponseMessageDTO> result = await _messageService.SendMessageAsync(messageDto, conversationId);
 
-            return new ApiResponse<List<Message>>(200, "Messages fetched successfully!", messagesResult.Data);
-        }
-        catch (Exception err)
-        {
-            Logger.LogError("Error getting users messages", err);
-            return new ApiResponse<List<Message>>(400, "An error occur when get messages");
-        }
+        if (result.IsSuccess)
+            return new ApiResponse<ResponseMessageDTO>(200, result.Message, result.Data);
+        
+        return new ApiResponse<ResponseMessageDTO>(500, result.Message);
     }
 }
