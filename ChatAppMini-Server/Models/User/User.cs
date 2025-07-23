4230928404bc8cecc1cdd0236a6e29586d9e91c7
KrySoSad <duythuc070205@gmail.com>
2025-07-23 12:10:16 +0700

    feat: add EmailAddress attribute to Email property in User model for validation

--- ChatAppMini-Server/Models/User/User.cs
+++ ChatAppMini-Server/Models/User/User.cs
@@ -5,6 +5,7 @@ public class User
     [Key]
     public Guid Id { get; set; } = Guid.NewGuid();
     public string Name { get; set; } = string.Empty;
+    [EmailAddress]
     public string Email { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
