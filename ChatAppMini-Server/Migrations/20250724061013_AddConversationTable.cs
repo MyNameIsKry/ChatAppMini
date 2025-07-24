492df40a72de652cc304c417445f840e36143c2e
KrySoSad <duythuc070205@gmail.com>
2025-07-24 13:11:47 +0700

    feat: add migration for Conversations, Messages, and ConversationUsers tables with updated foreign keys

---
+++ ChatAppMini-Server/Migrations/20250724061013_AddConversationTable.cs
@@ -0,0 +1,222 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChatAppMini.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddConversationTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ConversationUser_Conversation_ConversationId",
+                table: "ConversationUser");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ConversationUser_Users_UserId",
+                table: "ConversationUser");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Message_Conversation_ConversationId",
+                table: "Message");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Message_Users_SenderId",
+                table: "Message");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Message",
+                table: "Message");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ConversationUser",
+                table: "ConversationUser");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Conversation",
+                table: "Conversation");
+
+            migrationBuilder.RenameTable(
+                name: "Message",
+                newName: "Messages");
+
+            migrationBuilder.RenameTable(
+                name: "ConversationUser",
+                newName: "ConversationUsers");
+
+            migrationBuilder.RenameTable(
+                name: "Conversation",
+                newName: "Conversations");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Message_SenderId",
+                table: "Messages",
+                newName: "IX_Messages_SenderId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Message_ConversationId",
+                table: "Messages",
+                newName: "IX_Messages_ConversationId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_ConversationUser_ConversationId",
+                table: "ConversationUsers",
+                newName: "IX_ConversationUsers_ConversationId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Messages",
+                table: "Messages",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ConversationUsers",
+                table: "ConversationUsers",
+                columns: new[] { "UserId", "ConversationId" });
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Conversations",
+                table: "Conversations",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ConversationUsers_Conversations_ConversationId",
+                table: "ConversationUsers",
+                column: "ConversationId",
+                principalTable: "Conversations",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ConversationUsers_Users_UserId",
+                table: "ConversationUsers",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_Conversations_ConversationId",
+                table: "Messages",
+                column: "ConversationId",
+                principalTable: "Conversations",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_Users_SenderId",
+                table: "Messages",
+                column: "SenderId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ConversationUsers_Conversations_ConversationId",
+                table: "ConversationUsers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ConversationUsers_Users_UserId",
+                table: "ConversationUsers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_Conversations_ConversationId",
+                table: "Messages");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_Users_SenderId",
+                table: "Messages");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Messages",
+                table: "Messages");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ConversationUsers",
+                table: "ConversationUsers");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Conversations",
+                table: "Conversations");
+
+            migrationBuilder.RenameTable(
+                name: "Messages",
+                newName: "Message");
+
+            migrationBuilder.RenameTable(
+                name: "ConversationUsers",
+                newName: "ConversationUser");
+
+            migrationBuilder.RenameTable(
+                name: "Conversations",
+                newName: "Conversation");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Messages_SenderId",
+                table: "Message",
+                newName: "IX_Message_SenderId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Messages_ConversationId",
+                table: "Message",
+                newName: "IX_Message_ConversationId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_ConversationUsers_ConversationId",
+                table: "ConversationUser",
+                newName: "IX_ConversationUser_ConversationId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Message",
+                table: "Message",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ConversationUser",
+                table: "ConversationUser",
+                columns: new[] { "UserId", "ConversationId" });
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Conversation",
+                table: "Conversation",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ConversationUser_Conversation_ConversationId",
+                table: "ConversationUser",
+                column: "ConversationId",
+                principalTable: "Conversation",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ConversationUser_Users_UserId",
+                table: "ConversationUser",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Message_Conversation_ConversationId",
+                table: "Message",
+                column: "ConversationId",
+                principalTable: "Conversation",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Message_Users_SenderId",
+                table: "Message",
+                column: "SenderId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
