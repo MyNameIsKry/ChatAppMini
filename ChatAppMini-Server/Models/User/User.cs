d97f3e4d76809d4aac051a82736c7797f4ed226a
KrySoSad <duythuc070205@gmail.com>
2025-09-10 16:04:01 +0700

    feat: add default avatar URL for user profile

--- ChatAppMini-Server/Models/User/User.cs
+++ ChatAppMini-Server/Models/User/User.cs
@@ -8,6 +8,7 @@ public class User
     [EmailAddress]
     public string Email { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
+    public string AvatarUrl { get; set; } = "https://i.pinimg.com/736x/bc/43/98/bc439871417621836a0eeea768d60944.jpg";
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
     public List<ConversationUser> Conversations { get; set; } = new();
